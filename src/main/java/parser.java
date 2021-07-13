83a862839394ce7747a2444fa1505b34cdb0a33b
Sergei <kiselevcergei21@yandex.ru>
2021-07-13 13:12:30 +0400

    Вывел ссылки и  сделал изменения строчек

--- src/main/java/parser.java
+++ src/main/java/parser.java
@@ -9,24 +9,46 @@ import java.net.URL;
 public class parser {
 
     private static Object Element;
+    private  static int counter = 1;
+    private static org.jsoup.nodes.Element cell;
 
-    private static Document getPage()throws IOException{
+    private static Document getPage() throws IOException {
         String url = "https://poezdato.net/raspisanie-po-stancyi/samara/elektrichki/";
-        Document page = Jsoup.parse(new URL(url),3000);
+        Document page = Jsoup.parse(new URL(url), 15000);
         return page;
     }
-
-    public static void main(String[]args) throws IOException {
-        Document page= getPage();
+    public static void main(String[] args) throws IOException {
+        Document page = getPage();
         //css query language
         Element tbody = page.select("tbody").first();
         Elements rows = tbody.select("tr");
         Elements cells = tbody.select("td");
 
-        for(Element cell : cells ) {
-            String cellText = cell.text();
-            String cellUrl = cell.attr("href");
-            System.out.println(cellText);
+        for (Element cell : cells) {
+
+            if (counter == 6){
+                System.out.println("Отправление - " + cell.text());
+            }
+            if (counter == 5){
+                System.out.println("Стоянка - " + cell.text());
+            }
+            if (counter == 4){
+                System.out.println("Прибытие - " + cell.text());
+            }
+            if (counter == 3){
+                System.out.println("Маршрут - " + cell.text());
+            }
+            if (counter == 2) {
+                System.out.println("Номер элки - " + cell.text());
+                System.out.println("Ссылка на маршрут - " + cell.select("a").first().attr("href"));
+            }
+            if (counter == 8) {
+                System.out.println("");
+                counter = 0;
+            }
+            counter++;
         }
     }
 }
+
+
